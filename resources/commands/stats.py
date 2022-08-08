4b7f8d78d34af1f80a551c8d2f80186244b33268
Fortes <fortesa@spa1.gba.gov.ar>
2022-08-08 11:26:42 -0300

    replace reply_text to send_message

--- resources/commands/stats.py
+++ resources/commands/stats.py
@@ -5,79 +5,79 @@ from resources.commands.commandsMenu import defineLogs
 # Stats
 def bolsonaro(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Bolsonaro")
-    update.message.reply_text(info_Player("psn", "agu_q1988", "El Peluca Milei"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "agu_q1988", "El Peluca Milei"))
 
 
 def bolsonarokd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Bolsonaro KD")
-    update.message.reply_text(info_Player_kd("psn", "agu_q1988", "El Peluca Milei"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "agu_q1988", "El Peluca Milei"))
 
 
 def elch000(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Elch000")
-    update.message.reply_text(info_Player("psn", "Elch000", "Berisso1"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "Elch000", "Berisso1"))
 
 
 def elch000kd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Elch000 KD")
-    update.message.reply_text(info_Player_kd("psn", "Elch000", "Berisso1"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "Elch000", "Berisso1"))
 
 
 def hormigator(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Hormigator")
-    update.message.reply_text(info_Player("psn", "Hormigator1", "Hormigator"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "Hormigator1", "Hormigator"))
 
 
 def hormigatorkd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Hormigator KD")
-    update.message.reply_text(info_Player_kd("psn", "Hormigator1", "Hormigator"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "Hormigator1", "Hormigator"))
 
 
 def leko(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Leko")
-    update.message.reply_text(info_Player("psn", "Aleko22lp", "Leko"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "Aleko22lp", "Leko"))
 
 
 def lekokd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Leko KD")
-    update.message.reply_text(info_Player_kd("psn", "Aleko22lp", "Leko"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "Aleko22lp", "Leko"))
 
 
 def luquitas(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Luquitas")
-    update.message.reply_text(info_Player("psn", "luquitasc70", "LuquitasC70"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "luquitasc70", "LuquitasC70"))
 
 
 def luquitaskd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Luquitas KD")
-    update.message.reply_text(info_Player_kd("psn", "luquitasc70", "LuquitasC70"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "luquitasc70", "LuquitasC70"))
 
 
 def mandalorian(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Mandalorian")
-    update.message.reply_text(info_Player("battle", "Mandalorian%2311726", "MandaloriaN"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("battle", "Mandalorian%2311726", "MandaloriaN"))
 
 
 def mandaloriankd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Mandalorian KD")
-    update.message.reply_text(info_Player_kd("battle", "Mandalorian%2311726", "ElFisgonMorboson"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("battle", "Mandalorian%2311726", "ElFisgonMorboson"))
 
 
 def pablo(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Pablo")
-    update.message.reply_text(info_Player("psn", "carripablin10", "Carripablin10"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("psn", "carripablin10", "Carripablin10"))
 
 
 def pablokd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Pablo KD")
-    update.message.reply_text(info_Player_kd("psn", "carripablin10", "Carripablin10"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("psn", "carripablin10", "Carripablin10"))
 
 
 def rapax(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Rapax7")
-    update.message.reply_text(info_Player("battle", "rapax7%231438", "Rapax7"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player("battle", "rapax7%231438", "Rapax7"))
 
 
 def rapaxkd(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Rapax7 KD")
-    update.message.reply_text(info_Player_kd("battle", "rapax7%231438", "Rapax7"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=info_Player_kd("battle", "rapax7%231438", "Rapax7"))
--- resources/commands/weapons.py
+++ resources/commands/weapons.py
@@ -5,49 +5,49 @@ from ..api.guarsonapi import getWeaponFromApi
 # ARMAS
 def ak47(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por AK-47")
-    update.message.reply_text("AK-47:\n\n-Para AK-47 de ColdWar /AK47CW\n-Para AK-47 de Modern Warfare /AK47MW")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="AK-47:\n\n-Para AK-47 de ColdWar /AK47CW\n-Para AK-47 de Modern Warfare /AK47MW")
 
 
 def aug(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por AUG")
-    update.message.reply_text("AUG:\n\n-Para AUG de ColdWar /AUGCW\n-Para AUG de Modern Warfare /AUGMW")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="AUG:\n\n-Para AUG de ColdWar /AUGCW\n-Para AUG de Modern Warfare /AUGMW")
 
 
 def kar98(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Kar98")
-    update.message.reply_text("Kar98k:\n\n-Para Kar98k de Modern Warfare /Kar98MW\n-Para Kar98k de Vanguard /Kar98VG")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Kar98k:\n\n-Para Kar98k de Modern Warfare /Kar98MW\n-Para Kar98k de Vanguard /Kar98VG")
 
 
 def m1(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por M1 Garand")
-    update.message.reply_text(getWeaponFromApi('M1Garand'))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=getWeaponFromApi('M1Garand'))
 
 
 def m4(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por M4")
-    update.message.reply_text("M4:\n\n-Para M4 de ColdWar /XM4\n-Para M4 de Modern Warfare /M4A1")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="M4:\n\n-Para M4 de ColdWar /XM4\n-Para M4 de Modern Warfare /M4A1")
 
 
 def mp5(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por MP5")
-    update.message.reply_text("MP5:\n\n-Para MP5 de ColdWar /MP5CW\n-Para MP5 de Modern Warfare /MP5MW")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="MP5:\n\n-Para MP5 de ColdWar /MP5CW\n-Para MP5 de Modern Warfare /MP5MW")
 
 
 def p1911(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por 1911")
-    update.message.reply_text("1911:\n\n-Para 1911 de ColdWar /1911CW\n-Para 1911 de Modern Warfare /1911MW\n-Para 1911 de Vanguard /1911VG")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="1911:\n\n-Para 1911 de ColdWar /1911CW\n-Para 1911 de Modern Warfare /1911MW\n-Para 1911 de Vanguard /1911VG")
 
 
 def ppsh(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por PPSH")
-    update.message.reply_text("PPSH-41:\n\n-Para PPSH-41 de ColdWar /PPSHCW\n-Para PPSH-41 de Vanguard /PPSHVG")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="PPSH-41:\n\n-Para PPSH-41 de ColdWar /PPSHCW\n-Para PPSH-41 de Vanguard /PPSHVG")
 
 
 def type(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Type")
-    update.message.reply_text("Type:\n\n-Para Type 11 de Vanguard /Type11\n-Para Type 63 de ColdWar /Type63\n-Para Type 99 de Vanguard /Type99\n-Para Type 100 de Vanguard /Type100")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Type:\n\n-Para Type 11 de Vanguard /Type11\n-Para Type 63 de ColdWar /Type63\n-Para Type 99 de Vanguard /Type99\n-Para Type 100 de Vanguard /Type100")
 
 
 def vargo(update, context):
     defineLogs().info(f"El usuario {update.effective_user['username']}, consulto por Vargo 52")
-    update.message.reply_text("Vargo:\n\n-Para Vargo 52 de Coldwar /Vargo52\n-Para Vargo-S de Vanguard /VargoS")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Vargo:\n\n-Para Vargo 52 de Coldwar /Vargo52\n-Para Vargo-S de Vanguard /VargoS")
